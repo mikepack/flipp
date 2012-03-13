df13957a113bb8923445e7beea8fffc4a3675bdc
Mike Pack <mikepackdev@gmail.com>
2012-03-12 22:41:10 -0600

    Proper scope

--- flipp.gemspec
+++ flipp.gemspec
@@ -5,7 +5,7 @@ require 'bundler'
 
 Gem::Specification.new do |s|
   s.name        = "flipp"
-  s.version     = '0.0.2.1'
+  s.version     = '0.0.2.2'
   s.platform    = Gem::Platform::RUBY
   s.authors     = ["Mike Pack"]
   s.email       = ["mikepackdev@gmail.com"]
--- lib/flipp/tasks.rb
+++ lib/flipp/tasks.rb
@@ -4,12 +4,12 @@ namespace :flipp do
   desc 'Install flipp'
   task :install do
     # Move git hook into place
-    Flipp::GitHelper.install_hook
+    Flipp::Helpers::GitHelper.install_hook
   end
 
   desc 'Uninstall flipp'
   task :uninstall do
     # Remove git hook
-    Flipp::GitHelper.uninstall_hook
+    Flipp::Helpers::GitHelper.uninstall_hook
   end
 end
