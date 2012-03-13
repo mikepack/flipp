0c12fe5cb9a8478a8405c05ea0a032da27fd6cfb
Mike Pack <mikepackdev@gmail.com>
2012-03-12 22:31:55 -0600

    Fix scope

--- lib/flipp/railtie.rb
+++ lib/flipp/railtie.rb
@@ -3,15 +3,15 @@ require 'flipp'
 module Flipp
   class Railtie < Rails::Railtie
     initializer "restablish_connection.configure_rails_initialization" do
-      if Rails.env.development? and Flipp::GitHelper.hook_installed?
+      if Rails.env.development? and ::Flipp::Helpers::GitHelper.hook_installed?
         # Use the connection determined by flipp
         flipp = Flipp::Flipp.new
         flipp.switch_databases
       end
+    end
 
-      rake_tasks do
-        load 'flipp/tasks.rb'
-      end
+    rake_tasks do
+      load 'flipp/tasks.rb'
     end
   end
 end
